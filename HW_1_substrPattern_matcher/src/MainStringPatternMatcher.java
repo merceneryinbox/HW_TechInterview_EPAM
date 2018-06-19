73a9e68075c04bdc62e0a1d279d3f5fd6f1af3e2
merceneryru <mercenery@inbox.ru>
2018-06-19 09:41:52 +0300

    last commit done

--- HW_1_substrPattern_matcher/src/MainStringPatternMatcher.java
+++ HW_1_substrPattern_matcher/src/MainStringPatternMatcher.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
 * Дано:
@@ -20,28 +22,32 @@ public class MainStringPatternMatcher {
     private static String givenPattern;
     private static String givenStringToMatchExamine;
     private static int numOfMatches;
+    private static int pointer = 0;
 
     public MainStringPatternMatcher(String givenPattern, String givenStringToMatchExamine) {
-        this.givenPattern = givenPattern;
-        this.givenStringToMatchExamine = givenStringToMatchExamine;
+        MainStringPatternMatcher.givenPattern = givenPattern;
+        MainStringPatternMatcher.givenStringToMatchExamine = givenStringToMatchExamine;
     }
 
+    // it just for simple test
     public static void main(String[] args) {
-        // needed behavior
 
         int howMatchIsMatchesInGivenString = giveMeNumberOfMatches();
-
+        System.out.println("We have " + howMatchIsMatchesInGivenString + " matches and permutes in " +
+                           givenStringToMatchExamine + ".");
     }
 
     private static int giveMeNumberOfMatches() {
-        if (checkIncomes(givenPattern, givenStringToMatchExamine)) {
-
-            char[] patternCharArray = givenPattern.toCharArray();
-
-            List<char[]> devidedToCharArrrayStringList = devideExamineStringIntoArrays(givenStringToMatchExamine);
-            for (char[] testSubArr : devidedToCharArrrayStringList) {
-                if (givenPattern.equals(Arrays.toString(testSubArr))) {
-                    numOfMatches++;
+        while (hasNextSubArrFromMatchString(givenStringToMatchExamine)) {
+            if (checkBoundaries(givenPattern, nextSubArrFromMatchString(givenStringToMatchExamine))) {
+                pointer++;
+                List<char[]> devidedToCharArrrayStringList = devideExamineStringIntoArrays(givenStringToMatchExamine);
+                for (char[] testSubArr : devidedToCharArrrayStringList) {
+                    if (givenPattern.equals(Arrays.toString(testSubArr))) {
+                        numOfMatches++;
+                    } else if (permuteEquality(givenPattern, testSubArr)) {
+                        numOfMatches++;
+                    }
                 }
             }
         }
@@ -49,15 +55,51 @@ public class MainStringPatternMatcher {
         return numOfMatches;
     }
 
+    private static boolean hasNextSubArrFromMatchString(String givenStringToMatchExamine) {
+        return pointer + givenPattern.length() > givenStringToMatchExamine.length();
+    }
+
+    private static String nextSubArrFromMatchString(String givenStringToMatchExamine) {
+        return givenStringToMatchExamine.substring(pointer, pointer + givenPattern.length());
+    }
+
+    private static boolean permuteEquality(String givenPattern, char[] givenSubStringCharArForExamine) {
+        boolean result = false;
+        Map<Character, Integer> givenPatternMap = new HashMap();
+        Map<Character, Integer> testSubArrMap = new HashMap();
+        char[] givenPatternArray = givenPattern.toCharArray();
+        fulfillMapFromString((HashMap<Character, Integer>) givenPatternMap, givenPatternArray);
+        fulfillMapFromString((HashMap<Character, Integer>) testSubArrMap, givenSubStringCharArForExamine);
+
+        for (char ch : givenSubStringCharArForExamine) {
+            if (!givenPatternMap.containsKey(ch)) {
+                return false;
+            } else if (givenPatternMap.get(ch)!=testSubArrMap.get(ch)){
+                return false;
+            }
+        } return result;
+    }
+
+    private static void fulfillMapFromString(HashMap<Character, Integer> givenPatternMap, char[] givenPatternArray) {
+        for (char testChar : givenPatternArray) {
+            if (givenPatternMap.containsKey(testChar)) {
+                givenPatternMap.put(testChar, (givenPatternMap.get(testChar) + 1));
+            }
+        }
+    }
+
     private static List<char[]> devideExamineStringIntoArrays(String givenString) {
         List<char[]> backListCharArray = new ArrayList<>();
         for (int i = 0; i < givenString.length(); i++) {
-            backListCharArray.add((givenString.substring(i, i + givenPattern.length())).toCharArray());
+            int end = i + givenPattern.length();
+            if ((i + end) < givenString.length()) {
+                backListCharArray.add((givenString.substring(i, i + givenPattern.length())).toCharArray());
+            }
         }
-        return null;
+        return backListCharArray;
     }
 
-    private static boolean checkIncomes(String givenPattern, String givenStringToMatchExamine) {
+    private static boolean checkBoundaries(String givenPattern, String givenStringToMatchExamine) {
 
         return givenPattern.length() <= givenStringToMatchExamine.length();
     }
